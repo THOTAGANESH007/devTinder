c9090ec09e609f3f6215ee54cba956bf910c7c2b
THOTAGANESH007 <rr200007@rguktrkv.ac.in>
2025-05-16 20:15:20 +0530

    played with routes

--- Backend/app.js
+++ Backend/app.js
@@ -6,21 +6,45 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/test", (req, res) => {
-  res.send("Hello from the server");
-});
+// app.get("/test", (req, res) => {
+//   res.send("Hello from the server");
+// });
 
-app.get("/hello", adminAuth, (req, res) => {
-  res.send("Hello from the server Admin");
-});
+// app.get("/hello", adminAuth, (req, res) => {
+//   res.send("Hello from the server Admin");
+// });
 
-app.get("/tes", (req, res) => {
-  res.send("Test from the server");
-});
+// app.get("/tes", (req, res) => {
+//   res.send("Test from the server");
+// });
 
 // app.use("/", (req, res) => {
 //   res.status(404).send("Route not found");
 // });
+
+app.use(
+  "/test",
+  (req, res, next) => {
+    console.log("Middleware executed 1");
+    //res.send("Hello from the server 1");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Middleware executed 2");
+    //res.send("Hello from the server 2");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Middleware executed 3");
+    //res.send("Hello from the server 3");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Middleware executed 4");
+    res.send("Hello from the server 4");
+    next();
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`Server is Listening to the port ${PORT}`);
