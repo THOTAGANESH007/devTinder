517a445ed517bebd04707380b8fb925ed05bf717
THOTAGANESH007 <rr200007@rguktrkv.ac.in>
2025-05-17 15:53:56 +0530

    /getUser & /feed

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -7,7 +7,7 @@ export const registerUser = async (req, res) => {
       message: "Please fill all the fields",
     });
   }
-  alreadyExists = await UserModel.findOne({ email });
+  const alreadyExists = await UserModel.findOne({ email });
   if (alreadyExists) {
     return res.status(400).json({
       message: "User already exists",
@@ -27,5 +27,49 @@ export const registerUser = async (req, res) => {
     });
   } catch (error) {
     return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const getOneUser = async (req, res) => {
+  const { email } = req.body;
+  if (!email) {
+    return res.status(400).json({
+      message: "Please provide an email",
+    });
+  }
+  try {
+    const user = await UserModel.findOne({ email });
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+    return res.status(200).json({
+      message: "User found",
+      data: user,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = await UserModel.find();
+    if (!users) {
+      return res.status(404).json({
+        message: "No users found",
+      });
+    }
+    return res.status(200).json({
+      message: "Users found",
+      data: users,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server error",
+    });
   }
 };
--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -1,7 +1,13 @@
 import express from "express";
-import { registerUser } from "../controllers/userController.js";
+import {
+  getAllUsers,
+  getOneUser,
+  registerUser,
+} from "../controllers/userController.js";
 const userRoute = express.Router();
 
 userRoute.post("/register", registerUser);
+userRoute.get("/getUser", getOneUser);
+userRoute.get("/feed", getAllUsers);
 
 export default userRoute;
