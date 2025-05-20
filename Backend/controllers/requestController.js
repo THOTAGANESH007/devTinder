f56900b5e2dd40b32a049ea9efd155e4861db5d2
THOTAGANESH007 <rr200007@rguktrkv.ac.in>
2025-05-20 23:36:33 +0530

    review connection

--- Backend/controllers/requestController.js
+++ Backend/controllers/requestController.js
@@ -51,3 +51,46 @@ export const sendConnection = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const reviewConnection = async (req, res) => {
+  try {
+    const { status, requestId } = req.params;
+    const userId = req.user._id;
+    console.log(status, requestId, userId);
+    // Check if the status is valid
+    const validStatuses = ["accepted", "rejected"];
+    if (!validStatuses.includes(status)) {
+      return res.status(400).json({ message: "Invalid status." });
+    }
+
+    // Find the connection request
+    const connectionRequest = await ConnectionRequestModel.findOne({
+      _id: requestId,
+      toUserId: userId,
+      status: "interested",
+    });
+    console.log(connectionRequest);
+    if (!connectionRequest) {
+      return res.status(404).json({ message: "Connection request not found." });
+    }
+
+    //  Check if the user is authorized to review this request
+    // if (!connectionRequest.toUserId.equals(userId)) {
+    //   return res
+    //     .status(403)
+    //     .json({ message: "You are not authorized to review this request." });
+    // }
+
+    // Update the status of the connection request
+    connectionRequest.status = status;
+    await connectionRequest.save();
+
+    res.status(200).json({
+      message: `Connection request ${status} successfully.`,
+      connectionRequest,
+    });
+  } catch (error) {
+    console.error("Error in reviewConnection:", error);
+    res.status(500).json({ message: error.message });
+  }
+};
--- Backend/routes/requestRoute.js
+++ Backend/routes/requestRoute.js
@@ -1,6 +1,10 @@
 import express from "express";
 import userAuth from "../middlewares/auth.js";
-import { sendConnection } from "../controllers/requestController.js";
+import {
+  reviewConnection,
+  sendConnection,
+} from "../controllers/requestController.js";
 const requestRoute = express.Router();
 requestRoute.post("/send/:status/:toUserId", userAuth, sendConnection);
+requestRoute.post("/review/:status/:requestId", userAuth, reviewConnection);
 export default requestRoute;
